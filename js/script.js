1c3f5fdd3ac0cc3c4b3890458bf772ac26fad6db
Austin Taylor <Atay1109@gmail.com>
2024-05-20 11:52:46 -0500

    Added Popular movies API and pulled movie information to the DOM

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+        ${
+          movie.poster_path
+            ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+        />`
+            : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${movie.title}"
+    />`
+        }
+        </a>
+        <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+        </div>`;
+
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+// Fetch Data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = "7b1cd89b9dcf853b382c67aefb7545b1";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+// highlight active link
+function highlightActiveLink() {
+  const navLinks = document.querySelectorAll(".nav-link");
+  navLinks.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// Init App
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      displayPopularMovies();
+      break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/movies-details.html":
+      console.log("Movie Details");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener("DOMContentLoaded", init);
